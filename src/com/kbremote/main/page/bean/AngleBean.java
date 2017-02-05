51525af4456db20bc1ca135f1a0db1749bc20261
tgrlocak <tugrulocak@gmail.com>
2017-02-05 22:23:09 +0300

    AngleBean updated

--- src/com/kbremote/main/page/bean/AngleBean.java
+++ src/com/kbremote/main/page/bean/AngleBean.java
@@ -1,5 +1,8 @@
 package com.kbremote.main.page.bean;
 
+import java.io.IOException;
+import java.net.SocketException;
+import java.net.UnknownHostException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -8,11 +11,15 @@ import javax.faces.bean.ViewScoped;
 
 import org.apache.commons.collections4.CollectionUtils;
 
+import Exceptions.ModbusException;
+import ModbusClient.ModbusClient;
+
 import com.kbremote.main.model.Angle;
+import com.kbremote.main.util.AddressUtils;
 
 @ManagedBean
 @ViewScoped
-public class AngleBean {
+public class AngleBean extends ModbusBean{
 
 	private List<Angle> angles;
 
@@ -29,5 +36,93 @@ public class AngleBean {
 		this.angles = angles;
 	}
 	
+	public Angle getAngle(String tabancaNo){
+		Angle angle = new Angle();
+		ModbusClient client = getClient();
+		
+		switch (tabancaNo) {
+		case "1":
+			angle.setTabanca("1. Tabanca");
+			
+			angle.setSistemAalfa(getModbusValue(client, AddressUtils.TABANCA_1_A_ALFA));
+			angle.setSistemAbeta(getModbusValue(client, AddressUtils.TABANCA_1_A_BETA));
+			angle.setSistemAh(getModbusValue(client, AddressUtils.TABANCA_1_A_YUKSEKLIK));
+			
+			angle.setSistemBalfa(getModbusValue(client, AddressUtils.TABANCA_1_B_ALFA));
+			angle.setSistemBbeta(getModbusValue(client, AddressUtils.TABANCA_1_B_BETA));
+			angle.setSistemBh(getModbusValue(client, AddressUtils.TABANCA_1_B_YUKSEKLIK));
+			
+			angle.setSistemCalfa(getModbusValue(client, AddressUtils.TABANCA_1_C_ALFA));
+			angle.setSistemCbeta(getModbusValue(client, AddressUtils.TABANCA_1_C_BETA));
+			angle.setSistemCh(getModbusValue(client, AddressUtils.TABANCA_1_C_YUKSEKLIK));
+			
+			break;
+		case "2":
+			angle.setTabanca("2. Tabanca");
+			
+			angle.setSistemAalfa(getModbusValue(client, AddressUtils.TABANCA_2_A_ALFA));
+			angle.setSistemAbeta(getModbusValue(client, AddressUtils.TABANCA_2_A_BETA));
+			angle.setSistemAh(getModbusValue(client, AddressUtils.TABANCA_2_A_YUKSEKLIK));
+			
+			angle.setSistemBalfa(getModbusValue(client, AddressUtils.TABANCA_2_B_ALFA));
+			angle.setSistemBbeta(getModbusValue(client, AddressUtils.TABANCA_2_B_BETA));
+			angle.setSistemBh(getModbusValue(client, AddressUtils.TABANCA_2_B_YUKSEKLIK));
+			
+			angle.setSistemCalfa(getModbusValue(client, AddressUtils.TABANCA_2_C_ALFA));
+			angle.setSistemCbeta(getModbusValue(client, AddressUtils.TABANCA_2_C_BETA));
+			angle.setSistemCh(getModbusValue(client, AddressUtils.TABANCA_2_C_YUKSEKLIK));
+			break;
+		case "3":
+			angle.setTabanca("3. Tabanca");
+			
+			angle.setSistemAalfa(getModbusValue(client, AddressUtils.TABANCA_3_A_ALFA));
+			angle.setSistemAbeta(getModbusValue(client, AddressUtils.TABANCA_3_A_BETA));
+			angle.setSistemAh(getModbusValue(client, AddressUtils.TABANCA_3_A_YUKSEKLIK));
+			
+			angle.setSistemBalfa(getModbusValue(client, AddressUtils.TABANCA_3_B_ALFA));
+			angle.setSistemBbeta(getModbusValue(client, AddressUtils.TABANCA_3_B_BETA));
+			angle.setSistemBh(getModbusValue(client, AddressUtils.TABANCA_3_B_YUKSEKLIK));
+			
+			angle.setSistemCalfa(getModbusValue(client, AddressUtils.TABANCA_3_C_ALFA));
+			angle.setSistemCbeta(getModbusValue(client, AddressUtils.TABANCA_3_C_BETA));
+			angle.setSistemCh(getModbusValue(client, AddressUtils.TABANCA_3_C_YUKSEKLIK));
+			break;
+		case "4":
+			angle.setTabanca("4. Tabanca");
+			
+			angle.setSistemAalfa(getModbusValue(client, AddressUtils.TABANCA_4_A_ALFA));
+			angle.setSistemAbeta(getModbusValue(client, AddressUtils.TABANCA_4_A_BETA));
+			angle.setSistemAh(getModbusValue(client, AddressUtils.TABANCA_4_A_YUKSEKLIK));
+			
+			angle.setSistemBalfa(getModbusValue(client, AddressUtils.TABANCA_4_B_ALFA));
+			angle.setSistemBbeta(getModbusValue(client, AddressUtils.TABANCA_4_B_BETA));
+			angle.setSistemBh(getModbusValue(client, AddressUtils.TABANCA_4_B_YUKSEKLIK));
+			
+			angle.setSistemCalfa(getModbusValue(client, AddressUtils.TABANCA_4_C_ALFA));
+			angle.setSistemCbeta(getModbusValue(client, AddressUtils.TABANCA_4_C_BETA));
+			angle.setSistemCh(getModbusValue(client, AddressUtils.TABANCA_4_C_YUKSEKLIK));
+			break;
+		default:
+			break;
+		}
+		disconnectClient();
+		return angle;
+	}
+	
+	public int getModbusValue(ModbusClient client, int address){
+		int value = 0;
+		
+		try {
+			if(client != null){
+				value = client.ReadHoldingRegisters(address, 1) != null ? client.ReadHoldingRegisters(address, 1)[0] : 0;
+			} else {
+				value = 0;
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			value = 0;
+		}
+		return value;
+	}
 	
 }
